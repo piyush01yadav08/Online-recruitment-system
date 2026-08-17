c19727f5af9888c5e80f86457af3b697a4149890
Piyush Yadav <2432354.piyush@gmail.com>
2026-08-17 23:31:50 +0530

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -14,15 +14,13 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 )
 
-userSchema.pre('validate', function normalizeUserPhone(next) {
+userSchema.pre('validate', function normalizeUserPhone() {
   if (this.phone) this.phone = normalizePhone(this.phone)
-  next()
 })
 
-userSchema.pre('save', async function hashPassword(next) {
-  if (!this.isModified('password')) return next()
+userSchema.pre('save', async function hashPassword() {
+  if (!this.isModified('password')) return
   this.password = await bcrypt.hash(this.password, 12)
-  next()
 })
 
 userSchema.methods.comparePassword = function comparePassword(candidatePassword) {
